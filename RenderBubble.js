6c31555f0fe7ab7308c0b8cfbee0bd3abcc15f63
CodeSenpii <superdupa_fly@yahoo.com>
2020-08-03 20:50:26 -0400

    Update RenderBubble.js
    mouse over fill

--- RenderBubble.js
+++ RenderBubble.js
@@ -65,9 +65,11 @@ function createBubbles(selector, rawData) {
         .attr('stroke-width', 0.5)
         .on('mouseover', d => {
             createToolTip(d);
+            d3.select(this).style("fill", "red");
         })
         .on('mouseout', () => {
             d3.select('.tooltip-bubble').style('display', 'none');
+            d3.select(this).style("fill", d => d.color);
         });
 
     bubbles = bubbles.merge(bubblesE);
