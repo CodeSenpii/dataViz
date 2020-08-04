c50eae5b6aac5b45f1ef69e1e769e5939d1fbd31
CodeSenpii <superdupa_fly@yahoo.com>
2020-08-03 21:57:42 -0400

    Update RenderBubble.js
    bubble

--- RenderBubble.js
+++ RenderBubble.js
@@ -65,11 +65,11 @@ function createBubbles(selector, rawData) {
         .attr('stroke-width', 0.5)
         .on('mouseover', d => {
             createToolTip(d);
-            d3.select(this).attr("fill", "red");
+            d3.select(this).style('fill', 'red');
         })
         .on('mouseout', () => {
             d3.select('.tooltip-bubble').style('display', 'none');
-            d3.select(this).attr("fill", d => d.color);
+            d3.select(this).style('fill', d => d.color);
         });
 
     bubbles = bubbles.merge(bubblesE);
